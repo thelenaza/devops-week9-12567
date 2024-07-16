cc06b097209bb57ff8aef51ee2aede8d8d846faa
Your Namethelenaza <kaxtie07@gmail.com>
2024-07-16 13:54:32 +0700

    first commit

--- index.js
+++ index.js
@@ -5,15 +5,12 @@ const path = require('path')
 
 const PORT = process.env.PORT || 3001
 
+app.set("view engine", 'ejs')
+app.set('views',path.resolve('./views'))
 app.use(express.static('public'))
 
-//httpt:/localhost:3000
-app.set("view engine",'ejs')
-app.set('views' , path.resolve('./views'))
-app.get('/',(req,res)=>{
-    return res.render('index')
-})
+app.use('/',require('./routes/index'))
 
-app.listen(PORT, () =>{
+app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`)
-}) 
+})
